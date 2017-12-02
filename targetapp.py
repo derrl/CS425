338726687d698d049fa1c81e5e8e784b93ffb782
root <root@localhost.localdomain>
2017-12-02 03:39:03 -0600

    Fixed a few things

--- targetapp.py
+++ targetapp.py
@@ -1,15 +1,30 @@
-from flask import request, render_template
-from flask_api import FlaskAPI, status
+from flask import request, render_template, redirect
+from flask_api import FlaskAPI, status, response
 import json
 from time import strftime, gmtime
 
 app = FlaskAPI(__name__)
 
 
+@app.route('/')
+def redirect_to_main():
+    redirect_to_main = redirect('/main')
+    response = app.make_response(redirect_to_main)
+    return response
+
 @app.route('/main')
 def mainpage():
-    return render_template('index-light.html')
+    if "auth_user" in request.cookies:
+        return render_template('after_sign_in_indexpage.html')
+    else:
+        return render_template('index_light.html')
 
+@app.route('/logout')
+def logout():
+    redirect_to_main = redirect('/main')
+    response = app.make_response(redirect_to_main)
+    response.set_cookie('auth_user', value='', expires=0)  # Add the username arg
+    return response
 
 @app.route('/stores')
 def storepage():
@@ -26,24 +41,67 @@ def cartpage():
     return render_template('cart.html')
 
 
+
+
+@app.route('/customer/signin', methods=['GET', 'POST'])
+def signinpage():
+    if request.method == 'GET':
+        return render_template('customer_sign_in.html')
+    if request.method == 'POST':
+        # SIGN IN LOGIC
+        redirect_to_main = redirect('/main')
+        response = app.make_response(redirect_to_main)
+        response.set_cookie('auth_user', value='')  # Add the username arg
+        return response
+
 @app.route('/signup')
 def signuppage():
     return render_template('customer_sign_up.html')
 
-
-@app.route('/signin')
-def signinpage():
-    return render_template('customer_Sign_in.html')
-
-
 @app.route('/signup/success')
 def successpage():
-    return render_template('Signup_Success.html')
+    return render_template('signup_success.html')
 
+@app.route('/test')
+def test():
+    redirect_to_main = redirect('/main')
+    response = app.make_response(redirect_to_main)
+    response.set_cookie('auth_user', value='test')  # Add the username arg
+    return response
 
 @app.route('/customer/account')
 def accountpage():
     return render_template('payment.html')
+
+
+@app.route('/employee/signin', methods=['GET', 'PUT'])
+def empSignin():
+    if request.method == 'GET':
+        return render_template('employee_sign_in.html')
+    if request.method == 'POST':
+        # SIGN IN LOGIC
+        redirect_to_main = redirect('/main')
+        response = app.make_response(redirect_to_main)
+        response.set_cookie('auth_user', value='')  # Add the username arg
+        response.set_cookie('employee', value='True')
+        return response
+
+@app.route('/employee/product')
+def productCheck():
+    if ("employee" in request.cookies) & (request.cookie["employee"] == "True"):
+        return render_template("employee_check_product.html")
+    else:
+        return "FORBIDDEN"
+
+@app.route('/employee/product/update')
+def productUpdate():
+    if ("employee" in request.cookies) & (request.cookie["employee"] == "True"):
+        return render_template("update_productinfo.html")
+    else:
+        return "FORBIDDEN"
+
+
+
 
 
 if __name__ == "__main__":
