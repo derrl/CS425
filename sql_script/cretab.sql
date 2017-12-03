7d1333493068bc8d3ffd222da44b8af5547aa570
derrl <lhzsdu@gmail.com>
2017-12-03 14:57:20 -0600

    some changes  in table product
    delete company, capacity attribute in table product,
    change all the price attributes datatype to numeric(10,2)
    change the order status to numeric(1), set defaulte 0, and 0 means not paid, 1 means paid.

--- sql_script/cretab.sql
+++ sql_script/cretab.sql
@@ -1,89 +1,65 @@
 
   CREATE TABLE CUSTOMERS                                            
-   (	ID CHAR(10) NOT NULL ,                                             
-	PASSWORD VARCHAR2(200) NOT NULL ,                                      
+   (	USER_ID CHAR(10) NOT NULL ,                                             
 	FIRST_NAME VARCHAR2(20) NOT NULL ,                                     
-	MIDDLE_NAME VARCHAR2(20),                                                    
-	LAST_NAME VARCHAR2(20) NOT NULL ,                                      
-	PHONE_NUMBER VARCHAR2(100),                                                  
-	DATE_OF_BIRTH DATE,                                                          
-	 PRIMARY KEY (ID)                                            
+	MIDDLE_NAME VARCHAR2(20),
+	LAST_NAME VARCHAR2(20) NOT NULL ,
+	PHONE_NUMBER VARCHAR2(20),
+	DATE_OF_BIRTH DATE,
+	 PRIMARY KEY (USER_ID)                                            
    );
                                                                                 
                                                                                 
                                                                                 
   CREATE TABLE ORDERS                                               
    (	ORDER_ID CHAR(10) NOT NULL ,                                       
-	ORDER_DATE VARCHAR2(20) NOT NULL ,                                     
-	TOTAL_PRICE VARCHAR2(20),                                                    
-	 PRIMARY KEY (ORDER_ID)                                
+	ORDER_DATE VARCHAR2(20) NOT NULL ,
+	TOTAL_PRICE NUMERIC(10,2),
+   	USER_ID CHAR(10) NOT NULL,
+	PAYMENT_STATUS NUMERIC(1) DEFAULT 0 NOT NULL,
+	 PRIMARY KEY (ORDER_ID) ,
+	 FOREIGN KEY (USER_ID)
+	 REFERENCES CUSTOMERS (USER_ID) ON DELETE CASCADE
    );
                                                                                 
-                                                                                
-                                                                                
 
-  CREATE TABLE HASORDER                                             
-   (	CUSTOMER_ID CHAR(10),                                                    
-	ORDER_ID CHAR(10),                                                           
-	 PRIMARY KEY (CUSTOMER_ID, ORDER_ID) , 
-	  FOREIGN KEY (CUSTOMER_ID)                         
-	  REFERENCES CUSTOMERS (ID) ON DELETE CASCADE,                              
-	 FOREIGN KEY (ORDER_ID)                            
-	  REFERENCES ORDERS (ORDER_ID) ON DELETE CASCADE                             
-   );                                                  
-                                                                                
-                                                                                
-                                                                                
   CREATE TABLE ACCOUNT                                              
    (	PASSWORD VARCHAR2(20) NOT NULL ,                                   
 	USERNAME VARCHAR2(20) NOT NULL ,                                       
-	 PRIMARY KEY (USERNAME)                                
-   );                                                  
-                                                                                
-                                                                                
-                                                                                
-  CREATE TABLE HASACCOUNT                                           
-   (	USERNAME VARCHAR2(20),                                                   
-	CUSTOMER_ID CHAR(10),                                                        
-	 PRIMARY KEY (USERNAME, CUSTOMER_ID) ,  
-	 FOREIGN KEY (USERNAME)                          
-	  REFERENCES ACCOUNT (USERNAME) ,                          
-	 FOREIGN KEY (CUSTOMER_ID)                       
-	  REFERENCES CUSTOMERS (ID)                                
-   );                                                  
-                                                                                
-                                                                                
-                                                                                
-  CREATE TABLE CART                                                 
-   (	TAX VARCHAR2(20),                                                        
-	CART_ID CHAR(10),                                                            
-	PRICE VARCHAR2(20),                                                          
-	SHIPPING_PRICE VARCHAR2(20),                                                 
-	 PRIMARY KEY (CART_ID)                                     
-   );                                                  
-                                                                                
-                                                                                 
-                                                                                
-  CREATE TABLE PRODUCT                                              
-   (	PRODUCT_ID CHAR(10),                                                     
-	PRODUCT_NAME VARCHAR2(20),                                                   
-	PRODUCT_PRICE VARCHAR2(20),                                                  
-	PRODUCT_COMPANY VARCHAR2(20),                                                
-	PRODUCT_CAPACITY VARCHAR2(20),                                               
-	EXPIRATION_DATE DATE,                                                        
-	UPC_CODE VARCHAR2(20),                                                       
-	 PRIMARY KEY (PRODUCT_ID)                               
+	USER_ID CHAR(10) ,
+	 PRIMARY KEY (USER_ID)                                
    );                                                  
                                                                                 
 
-                                                                                
+  CREATE TABLE CART                                                 
+   (	TAX NUMERIC(10,2),
+	PRICE NUMERIC(10,2),
+	SHIPPING_PRICE NUMERIC(10,2),
+   	USER_ID CHAR(10),
+	 PRIMARY KEY (USER_ID) ,
+	 FOREIGN KEY (USER_ID)                          
+	  REFERENCES CUSTOMERS (USER_ID) ON DELETE CASCADE 
+   );
+
+  CREATE TABLE PRODUCT                                              
+   (	PRODUCT_ID CHAR(10),
+	PRODUCT_NAME VARCHAR2(20),
+	PRODUCT_PRICE NUMERIC(10,2),
+--	PRODUCT_COMPANY VARCHAR2(20),
+--	PRODUCT_CAPACITY VARCHAR2(20),
+	EXPIRATION_DATE DATE,
+	UPC_CODE VARCHAR2(20),
+	 PRIMARY KEY (PRODUCT_ID)
+   );
+
+
   CREATE TABLE ORDERDETAIL                                          
-   (	ORDER_ID CHAR(10),                                                       
-	PRODUCT_ID CHAR(10),                                                         
-	COUNT VARCHAR2(20),                                                          
+   (	ORDER_ID CHAR(10),
+	PRODUCT_ID CHAR(10),
+	PRODUCT_NUMBER INT,
 	 PRIMARY KEY (ORDER_ID, PRODUCT_ID) ,
 	 FOREIGN KEY (ORDER_ID)                         
-	  REFERENCES ORDERS (ORDER_ID) ON DELETE CASCADE ,                           
+	  REFERENCES ORDERS  ON DELETE CASCADE ,
 	 FOREIGN KEY (PRODUCT_ID)                        
 	  REFERENCES PRODUCT (PRODUCT_ID)                          
    );                                                  
@@ -91,33 +67,23 @@
                                                                                 
                                                                                 
   CREATE TABLE CARTDETAIL                                           
-   (	CART_ID CHAR(10),                                                        
-	PRODUCT_ID CHAR(10),                                                         
-	 PRIMARY KEY (CART_ID, PRODUCT_ID) ,
-	 FOREIGN KEY (CART_ID)                           
-	  REFERENCES CART (CART_ID) ON DELETE CASCADE ,                              
+   (	USER_ID CHAR(10),
+	PRODUCT_ID CHAR(10),
+	PRODUCT_NUMBER INT,
+	 PRIMARY KEY (USER_ID, PRODUCT_ID) ,
+	 FOREIGN KEY (USER_ID)                           
+	  REFERENCES CART (USER_ID) ON DELETE CASCADE ,
 	 FOREIGN KEY (PRODUCT_ID)                        
 	  REFERENCES PRODUCT (PRODUCT_ID)                          
    );                                                  
                                                                                 
                                                                                 
-                                                                                
-  CREATE TABLE HASCART                                              
-   (	CUSTOMER_ID CHAR(10),                                                    
-	CART_ID CHAR(10),                                                            
-	 PRIMARY KEY (CUSTOMER_ID, CART_ID) ,
-	 FOREIGN KEY (CUSTOMER_ID)                          
-	  REFERENCES CUSTOMERS (ID) ON DELETE CASCADE ,                              
-	 FOREIGN KEY (CART_ID)                              
-	  REFERENCES CART (CART_ID)                                
-   );                                                  
-                                                                                
-                                                                                
+                                                                               
                                                                                 
 
   CREATE TABLE PRODUCT_TYPE                                         
-   (	CATEGORY_NAME VARCHAR2(20),                                              
-	DESCRIPTION VARCHAR2(20),                                                    
+   (	CATEGORY_NAME VARCHAR2(20),
+	DESCRIPTION VARCHAR2(20),
 	 PRIMARY KEY (CATEGORY_NAME)                       
    );                                                  
                                                                                 
@@ -125,11 +91,11 @@
                                                                                 
                                                                                 
   CREATE TABLE PRODUCTBELONGTO                                      
-   (	PRODUCT_ID CHAR(10),                                                     
-	CATEGORY_NAME VARCHAR2(20),                                                  
+   (	PRODUCT_ID CHAR(10),
+	CATEGORY_NAME VARCHAR2(20),
 	 PRIMARY KEY (PRODUCT_ID, CATEGORY_NAME) ,
 	 FOREIGN KEY (PRODUCT_ID)                   
-	  REFERENCES PRODUCT (PRODUCT_ID) ON DELETE CASCADE ,                        
+	  REFERENCES PRODUCT (PRODUCT_ID) ON DELETE CASCADE ,
 	 FOREIGN KEY (CATEGORY_NAME)                
 	  REFERENCES PRODUCT_TYPE (CATEGORY_NAME)                  
    );                                                  
@@ -137,9 +103,9 @@
                                                                                 
                                                                                 
   CREATE TABLE BRAND                                                
-   (	BRAND_NAME VARCHAR2(20),                                                 
-	BRAND_DESCRIPTION VARCHAR2(20),                                              
-	BRAND_COMPANY VARCHAR2(20),                                                  
+   (	BRAND_NAME VARCHAR2(20),
+	BRAND_DESCRIPTION VARCHAR2(20),
+	BRAND_COMPANY VARCHAR2(20),
 	 PRIMARY KEY (BRAND_NAME)                                 
    );                                                  
                                                                                 
@@ -147,11 +113,11 @@
                                                                                 
 
   CREATE TABLE BRANDBELONGTO                                        
-   (	PRODUCT_ID CHAR(10),                                                     
-	BRAND_NAME VARCHAR2(20),                                                     
+   (	PRODUCT_ID CHAR(10),
+	BRAND_NAME VARCHAR2(20),
 	 PRIMARY KEY (BRAND_NAME, PRODUCT_ID) ,
 	 FOREIGN KEY (PRODUCT_ID)                     
-	  REFERENCES PRODUCT (PRODUCT_ID) ON DELETE CASCADE ,                        
+	  REFERENCES PRODUCT (PRODUCT_ID) ON DELETE CASCADE ,
 	 FOREIGN KEY (BRAND_NAME)                     
 	  REFERENCES BRAND (BRAND_NAME)                            
    );                                                  
@@ -159,13 +125,13 @@
                                                                                 
                                                                                 
   CREATE TABLE STORE                                                
-   (	STORE_ID CHAR(10),                                                       
-	STORE_NAME VARCHAR2(20),                                                     
-	STORE_ADDRESS VARCHAR2(20),                                                        
-	PHONE_NUMBER VARCHAR2(20),                                                   
-	OPEN_TIME VARCHAR2(20),                                                      
-	CLOSE_TIME VARCHAR2(20),                                                     
-	DESCRIPTION VARCHAR2(20),                                                    
+   (	STORE_ID CHAR(10),
+	STORE_NAME VARCHAR2(20),
+	STORE_ADDRESS VARCHAR2(20),
+	PHONE_NUMBER VARCHAR2(20),
+	OPEN_TIME VARCHAR2(20),
+	CLOSE_TIME VARCHAR2(20),
+	DESCRIPTION VARCHAR2(20),
 	 PRIMARY KEY (STORE_ID)                                   
    );                                                  
                                                                                 
@@ -173,12 +139,12 @@
                                                                                 
                                                                                 
   CREATE TABLE INVENTORY                                            
-   (	STORE_ID CHAR(10),                                                       
-	PRODUCT_ID CHAR(10),                                                         
-	COUNT VARCHAR2(20),                                                          
+   (	STORE_ID CHAR(10),
+	PRODUCT_ID CHAR(10),
+	PRODUCT_NUMBER INT,
 	 PRIMARY KEY (STORE_ID, PRODUCT_ID) ,
 	 FOREIGN KEY (STORE_ID)                           
-	  REFERENCES STORE (STORE_ID) ON DELETE CASCADE ,                            
+	  REFERENCES STORE (STORE_ID) ON DELETE CASCADE ,
 	 FOREIGN KEY (PRODUCT_ID)                         
 	  REFERENCES PRODUCT (PRODUCT_ID) ON DELETE CASCADE
    );                                                  
@@ -186,49 +152,39 @@
                                                                                 
                                                                                 
   CREATE TABLE VENDORS                                              
-   (	VENDOR_NAME VARCHAR2(20),                                                
-	 PRIMARY KEY (VENDOR_NAME)                              
-   );                                                  
-                                                                                
-                                                                                
-                                                                                
-  CREATE TABLE SUPPLIES                                             
-   (	STORE_ID CHAR(10),                                                       
-	VENDOR_NAME VARCHAR2(20),                                                    
-	 PRIMARY KEY (STORE_ID, VENDOR_NAME) ,
+  (	STORE_ID CHAR(10),
+   	VENDOR_NAME VARCHAR2(20),
+	 PRIMARY KEY (STORE_ID) ,
 	 FOREIGN KEY (STORE_ID)                            
-	  REFERENCES STORE (STORE_ID) ,                            
-	 FOREIGN KEY (VENDOR_NAME)                         
-	  REFERENCES VENDORS (VENDOR_NAME)                         
+	  REFERENCES STORE (STORE_ID)
    );                                                  
-                                                                                
-                                                                                
-                                                                                
+
+
   CREATE TABLE PAYMENTMETHOD                                        
-   (	METHOD_ID CHAR(10),                                                      
-	HOLDER_NAME VARCHAR2(20),                                                    
-	CARD_NUMBER VARCHAR2(20),                                                    
-	EXPIRATION_DATE DATE,                                                        
-	SVC VARCHAR2(20),                                                            
-	CUSTOMER_ID CHAR(10),                                                        
-	 PRIMARY KEY (METHOD_ID) ,
-	 FOREIGN KEY (CUSTOMER_ID)                    
-	  REFERENCES CUSTOMERS (ID) ON DELETE CASCADE                                
+   (	METHOD_ID CHAR(10),
+	HOLDER_NAME VARCHAR2(20),
+	CARD_NUMBER VARCHAR2(20),
+	EXPIRATION_DATE DATE,
+	SVC VARCHAR2(20),
+	USER_ID CHAR(10),
+	 PRIMARY KEY (USER_ID) ,
+	 FOREIGN KEY (USER_ID)                    
+	  REFERENCES CUSTOMERS (USER_ID) ON DELETE CASCADE                                
    );                                                  
                                                                                 
 
                                                                                 
                                                                                 
   CREATE TABLE ADDRESS                                              
-   (	ADDRESS_ID CHAR(10),                                                     
-	ADDRESS_LINE_1 VARCHAR2(20),                                                 
-	ADDRESS_LINE_2 VARCHAR2(20),                                                 
-	CITY VARCHAR2(20),                                                           
-	ZIPCODE VARCHAR2(20),                                                        
-	CUSTOMER_ID CHAR(10),                                                        
-	STATE VARCHAR2(20),                                                          
+   (	ADDRESS_ID CHAR(10),
+	ADDRESS_LINE_1 VARCHAR2(20),
+	ADDRESS_LINE_2 VARCHAR2(20),
+	CITY VARCHAR2(20),
+	ZIPCODE VARCHAR2(20),
+	STATE VARCHAR2(20),
+	USER_ID CHAR(10),
 	 PRIMARY KEY (ADDRESS_ID) ,
-	 FOREIGN KEY (CUSTOMER_ID)                          
-	  REFERENCES CUSTOMERS (ID) ON DELETE CASCADE                                
+	 FOREIGN KEY (USER_ID)                          
+	  REFERENCES CUSTOMERS (USER_ID) ON DELETE CASCADE
    );                                                  
                                                                                 
